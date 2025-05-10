20517e950ddaaa6ced488eb1901851d15e5beea6
nickthelegend <testingtesla7@gmail.com>
2025-05-10 16:21:56 +0530

    Initial COmmit

---
+++ components/nav.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { ConnectWalletButton } from "./connect-wallet-button"
+import { ThemeToggle } from "./theme-toggle"
+
+export default function Nav() {
+  return (
+    <nav className="flex justify-between items-center p-4 border-b">
+      <h1 className="text-xl font-bold pl-20">AlgoBlink</h1>
+      <div className="flex items-center">
+        <ThemeToggle />
+        <ConnectWalletButton />
+      </div>
+    </nav>
+  )
+}
